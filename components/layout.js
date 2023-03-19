6f73b2b116aaaa75be225025a437912ff760a6a5
noli99 <noli.9@icloud.com>
2023-03-19 17:48:57 +0900

    データフェッチ

--- components/layout.js
+++ components/layout.js
@@ -28,15 +28,19 @@ export default function Layout({ children, home }) {
       <header className={styles.header}>
         {home ? (
           <>
-            <Image
-              priority
-              src="/images/sample.png"
-              className={utilStyles.borderCircle}
-              height={144}
-              width={144}
-              alt={name}
-            />
-            <h1 className={utilStyles.heading2Xl}>{name}</h1>
+            <Link href="/posts/first-post">
+              <Image
+                priority
+                src="/images/sample.png"
+                className={utilStyles.borderCircle}
+                height={144}
+                width={144}
+                alt={name}
+              />
+            </Link>
+            <h1 className={utilStyles.heading2Xl}>
+              <Link href="/posts/first-post">{name}</Link>
+            </h1>
           </>
         ) : (
           <>
